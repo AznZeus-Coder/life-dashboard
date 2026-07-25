ef07a5059ec13f2ff717667b56c6279bbb5f4e50
AznZeus-Coder <AznZeus-Coder@users.noreply.github.com>
2026-07-24 23:46:50 -0700

    fix: collapse account controls after sign in

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='daybook-v8';
-const ASSETS=['./','./index.html','./styles.css?v=8','./app.js?v=8','./manifest.webmanifest'];
+const CACHE='daybook-v9';
+const ASSETS=['./','./index.html','./styles.css?v=9','./app.js?v=9','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
