5aaf644ef7f670e24f7e4fbf24cd7f9c161d6e9d
AznZeus-Coder <AznZeus-Coder@users.noreply.github.com>
2026-07-25 07:53:34 -0700

    feat: apply pixel art cyberpunk dashboard theme

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='daybook-v11';
-const ASSETS=['./','./index.html','./styles.css?v=11','./app.js?v=11','./manifest.webmanifest'];
+const CACHE='daybook-v12';
+const ASSETS=['./','./index.html','./styles.css?v=12','./app.js?v=12','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
